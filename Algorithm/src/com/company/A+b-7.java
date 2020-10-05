70627885dd3d46569bd1f649704042959c305ecb
jeonka1001 <ruddks2009@naver.com>
2020-10-05 14:57:23 +0900

    A+B-7 문제

---
+++ Algorithm/src/com/company/A+b-7.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("data.txt"));
+		int T = Integer.parseInt(sc.nextLine());
+		for( int i = 0 ; i < T ; i ++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println("Case #"+(i+1)+": "+(A+B));
+        }
+    }
+}
