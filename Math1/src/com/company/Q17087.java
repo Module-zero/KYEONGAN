0bbd48ba526c1ff2174500eeee0a5cabeb0afa16
jeonka1001 <ruddks2009@naver.com>
2020-10-07 14:42:48 +0900

    2020_10_07 -1

---
+++ Math1/src/com/company/Q17087.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+public class Q17087 {
+    public static void main(String[] args)throws Exception{
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
+
+        String[] strArr = bf.readLine().split(" ");
+        int n = Integer.parseInt(strArr[0]);
+        long subinPos = Long.parseLong(strArr[1]);
+
+        String[] strArr1 = bf.readLine().split(" ");
+        long[] findPos = new long[n];
+        for(int i = 0 ;i < n ; i ++){
+            long tmp = subinPos - Long.parseLong(strArr1[i]);
+            findPos[i] = tmp > 0 ? tmp : -tmp ;
+        }
+        long answer = findPos[0];
+        for(int i = 1; i < n ; i ++){
+            long tmp = gcd(answer,findPos[i]);
+            answer = answer > tmp ? tmp : answer;
+        }
+        System.out.println(answer);
+    }
+
+    public static long gcd(long a, long b){
+        while(b!=0){
+            long r = a%b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
---
+++ Math1/src/com/company/Q9613.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+public class Q9613 {
+    public static void main(String[] args)throws Exception{
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
+
+        int t = Integer.parseInt(bf.readLine());
+
+        for(int i = 0 ; i < t ; i ++){
+            String[] strArr = bf.readLine().split(" ");
+            int n = Integer.parseInt(strArr[0]);
+            long answer = 0;
+            for(int j = 1; j < n ; j++){
+                long numA = Long.parseLong(strArr[j]);
+                for(int k = j+1; k<= n ; k++){
+                    long numB = Long.parseLong(strArr[k]);
+                    answer += gcd(numA,numB);
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+
+    public static long gcd(long a , long b ){
+        while(b!=0){
+            long r = a%b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
