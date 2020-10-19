4fa2e2a9527328828a87228fe27b86f2f816ec95
jeonka1001 <ruddks2009@naver.com>
2020-10-20 01:58:42 +0900

    2020_10_20 -1

---
+++ BruteForce_N&M/src/com/company/Q15666.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Q15666 {
+    static Set<String> list = new LinkedHashSet<>();
+    static int n ;
+    static int m ;
+    static int[] numArr;
+    static int[] answer ;
+    static StringBuilder sb= new StringBuilder();
+    public static void main(String[] args)throws Exception{
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
+
+        String[] strArr = bf.readLine().split(" ");
+        n = Integer.parseInt(strArr[0]);
+        m = Integer.parseInt(strArr[1]);
+        answer = new int[m];
+        numArr= new int[n];
+        String[] strArr1 = bf.readLine().split(" ");
+        for(int i = 0 ; i < n ; i ++){
+            numArr[i] = Integer.parseInt(strArr1[i]);
+        }
+        Arrays.sort(numArr);
+        select(0,0);
+        for(String str : list){
+            sb.append(str+"\n");
+        }
+        System.out.println(sb);
+    }
+    public static void select(int start,int cnt){
+        if(cnt == m){
+            String str = "";
+            for(int num : answer){
+                str+=num+" ";
+            }
+            list.add(str);
+            return;
+        }
+        for(int i = start; i < n ; i ++){
+            answer[cnt] = numArr[i];
+            select(i,cnt+1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
  - %(mod) 연산이 문제 조건에 있다면, answer 를 누적하는 연산을 할 때, ```answer += value%mod; ``` 가 아닌 ``` answer = (answer + value) % mod; ``` 로 해야하며, 모든 value 에 대해서 mod 연산을 해줘야 한다.
  - 알고리즘 설계시 최대 범위보다 크고, 최소 범위보다 작은 경우의 예외처리를 생각해야 한다.
  - **중복된 값을 제거** 하기 위해서는 ```Hash 계열``` 의 자료구조를 사용하면 편리하다.
+ - ```Arrays.sort(Array,startIdx,endIdx);``` 를 사용할 경우 ```startIdx``` 는 정렬 범위에 포함되나 ```endIdx``` 는 포함되지 않는다.  
+ 	따라서 ```endIdx``` 는 1 큰수를 넣어줘야 한다.
