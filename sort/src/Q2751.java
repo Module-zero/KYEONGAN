fd1789d1f48d60a1b851b3d1017530fa9136f858
jeonka1001 <ruddks2009@naver.com>
2020-12-03 17:28:20 +0900

    2020_12_3 -3

--- sort/src/Q2751.java
+++ sort/src/Q2751.java
@@ -4,48 +4,56 @@ import java.util.Stack;
 
 public class Q2751 {
     static int[] numArr;
+    static int[] tmpArr;
+    static int n;
     public static void main(String[] args)throws Exception{
 //        BufferedReader bf = new BufferedReader (new InputStreamReader(System.in));
         BufferedReader bf = new BufferedReader (new FileReader(new File("data.txt")));
 
-        int n = Integer.parseInt(bf.readLine());
+        n = Integer.parseInt(bf.readLine());
         numArr = new int[n];
+        tmpArr = new int[n];
         for(int i = 0 ; i < n ; i ++){
             numArr[i] = Integer.parseInt(bf.readLine());
         }
-        quickSort(numArr,0,n-1);
+        sort(0,n-1);
         StringBuilder sb = new StringBuilder();
         for(int i : numArr){
             sb.append(i).append("\n");
         }
         System.out.println(sb.toString());
     }
-    public static void quickSort(int[] numArr,int start,int end){
-        if(start < end) {
-            int pivot = partition(start, end);
-            quickSort(numArr, start, pivot - 1);
-            quickSort(numArr, pivot + 1, end);
+    public static void sort(int start,int end){
+        if(start == end){
+            return;
         }
+        int mid = (start+end)/2;
+        sort(start,mid);
+        sort(mid+1,end);
+        merge(start,end);
     }
-    public static int partition(int start, int end){
-        int front = start;
-        int pivot = (start + end) / 2;
-        int pivotValue = numArr[pivot];
-        swap(pivot,end);
-        for (int i = start; i < end; i++) {
-            if (numArr[i] < pivotValue) {
-                int tmp = numArr[front];
-                numArr[front] = numArr[i];
-                numArr[i] = tmp;
-                front ++;
+    public static void merge(int start, int end){
+        int mid = (start+end)/2;
+        int i = start;
+        int j = mid+1;
+        int k = 0 ;
+
+        while(i <= mid && j <= end){
+            if(numArr[i] < numArr[j]){
+                tmpArr[k++] = numArr[i++];
+            }
+            else{
+                tmpArr[k++] = numArr[j++];
             }
         }
-        swap(front,end);
-        return front;
-    }
-    public static void swap(int a, int b){
-        int tmp = numArr[a];
-        numArr[a] = numArr[b];
-        numArr[b] = tmp;
+        while (i <= mid){
+            tmpArr[k++] = numArr[i++];
+        }
+        while (j <= end){
+            tmpArr[k++] = numArr[j++];
+        }
+        for(int ii = start ; ii <= end; ii ++){
+            numArr[ii] = tmpArr[ii-start];
+        }
     }
 }
