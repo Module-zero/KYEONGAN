8777656fe3d0373607421550f6c05d1c7c418ba5
jeonka1001 <ruddks2009@naver.com>
2020-12-12 15:49:15 +0900

    2020_12_11 -2

---
+++ divide_and_conquer/src/Q10815.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+public class Q10815 {
+    public static void main(String[] args)throws Exception{
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
+
+        int
+    }
+}
