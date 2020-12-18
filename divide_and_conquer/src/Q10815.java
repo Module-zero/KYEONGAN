0e71391a3d6bcd160ab2f380857a21e31d6b037c
jeonka1001 <ruddks2009@naver.com>
2020-12-18 13:59:38 +0900

    12.18

--- divide_and_conquer/src/Q10815.java
+++ divide_and_conquer/src/Q10815.java
@@ -2,12 +2,54 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Q10815 {
     public static void main(String[] args)throws Exception{
 //        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
 
-        int
+        int have = Integer.parseInt(bf.readLine());
+        int[] haveCards = new int[have];
+        String[] haveStr = bf.readLine().split(" ");
+        for(int i = 0 ; i < have ; i++){
+            haveCards[i] = Integer.parseInt(haveStr[i]);
+        }
+        Arrays.sort(haveCards);
+
+        int cmpNum = Integer.parseInt(bf.readLine());
+        String[] cmpStr = bf.readLine().split(" ");
+        int[] cmpCard = new int[cmpNum];
+        for(int i = 0 ; i < cmpNum ; i ++){
+            cmpCard[i] = Integer.parseInt(cmpStr[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < cmpNum ; i ++){
+            int target = cmpCard[i];
+            int left = 0;
+            int right = have-1;
+            boolean arrive = false;
+            while(left <= right){
+                int mid = (left + right)/2;
+                if(target > haveCards[mid]){
+                    left = mid+1;
+                }
+                else if(target < haveCards[mid]){
+                    right = mid-1;
+                }
+                else{
+                    arrive = true;
+                    break;
+                }
+            }
+            if(arrive){
+                sb.append(1+" ");
+            }
+            else{
+                sb.append(0+" ");
+            }
+        }
+        System.out.println(sb);
     }
 }
