601fde75ac87473a035429e66d7d444b921e6d30
jeonka1001 <ruddks2009@naver.com>
2020-12-24 17:45:09 +0900

    2020-12-25 -1

---
+++ greedy/src/Q1744.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class Q1744 {
+    public static void main(String[] args)throws Exception{
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
+
+        int n = Integer.parseInt(bf.readLine());
+        LinkedList<Integer> plus = new LinkedList<>();
+        LinkedList<Integer> minus = new LinkedList<>();
+        int answer = 0 ;
+        for(int i = 0 ; i < n ; i ++){
+            int tmp = Integer.parseInt(bf.readLine());
+            if(tmp > 1){
+                plus.push(tmp);
+            }
+            else if(tmp <= 0){
+                minus.push(tmp);
+            }
+            else{
+                answer += tmp;
+            }
+        }
+        Collections.sort(minus);
+        Collections.sort(plus, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        while(minus.size() > 1 && plus.size() > 1){
+            int mFirst = minus.poll();
+            int pFirst = plus.poll();
+            int mSecond = minus.poll();
+            int pSecond = plus.poll();
+            answer += (mFirst * mSecond);
+            answer += (pFirst * pSecond);
+        }
+        while(!minus.isEmpty()){
+            int tmp = minus.poll();
+            if(!minus.isEmpty()){
+                int tmp2 = minus.poll();
+                answer += (tmp * tmp2);
+            }
+            else{
+                answer += tmp;
+            }
+        }
+        while(!plus.isEmpty()){
+            int tmp = plus.poll();
+            if(!plus.isEmpty()){
+                int tmp2 = plus.poll();
+                answer += (tmp * tmp2);
+            }
+            else{
+                answer += tmp;
+            }
+        }
+        System.out.println(answer);
+    }
+}
---
+++ greedy/src/Q2875.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+public class Q2875 {
+    public static void main(String[] args)throws Exception{
+//        BufferedReader bf = new BufferedReader (new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
+
+        String[] nmk = bf.readLine().split(" ");
+        int n = Integer.parseInt(nmk[0]);
+        int m = Integer.parseInt(nmk[1]);
+        int k = Integer.parseInt(nmk[2]);
+
+        int group = n/2;
+        if( group < m){
+            int tmp = (m-group) + (n%2);
+            if(tmp >=  k){
+                System.out.println(group);
+            }
+            else{
+                int tmp2 = k - tmp;
+                int intern = tmp2/3;
+                if(tmp2 % 3 == 0){
+                    System.out.println(group - intern);
+                }
+                else{
+                    System.out.println(group - intern - 1);
+                }
+            }
+        }
+        else if(group == m){
+            int tmp = k/3;
+            if(k%3 == 0){
+                System.out.println(group - tmp);
+            }
+            else{
+                System.out.println(group - tmp - 1);
+            }
+        }
+        else{
+            int tmp = n - (m*2);
+            if(tmp >= k){
+                System.out.println(m);
+            }
+            else{
+                int tmp2 = k - tmp;
+                int intern = tmp2/3;
+                if(tmp2%3 == 0){
+                    System.out.println(m - intern);
+                }
+                else{
+                    System.out.println(m - intern - 1);
+                }
+            }
+        }
+    }
+}
