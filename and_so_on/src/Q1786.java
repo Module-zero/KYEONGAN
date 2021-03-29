8e1626c801a086d9e8522a7e60f409788cbe9ac9
jeonka1001 <ruddks2009@naver.com>
2021-03-29 18:27:30 +0900

    2021-03-27 -1

--- and_so_on/src/Q1786.java
+++ and_so_on/src/Q1786.java
@@ -1,36 +1,60 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Q1786 {
-    static char[] T;
-    static char[] P;
-    static int[] fail;
-    static StringBuilder sb;
-    static int n ;
-    static int m;
     public static void main(String[] args)throws Exception{
-        BufferedReader bf = new BufferedReader(new FileReader(new File("data.txt")));
-//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        T = bf.readLine().toCharArray();
-        P = bf.readLine().toCharArray();
-        n = P.length;
-        m = T.length;
-        fail = new int[n];
-        failFunction();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        char[] T = bf.readLine().toCharArray();
+        char[] P = bf.readLine().toCharArray();
+        int pl = P.length;
+        int[] fail = new int[pl];
+        failFunction(P,fail);
+        solve(P,T,fail);
     }
-    public static void failFunction(){
-        for(int i = 1 ; i < n ; i ++){
-            int t = fail[i-1];
-            while(t > 0 && P[t] != P[i]) {
-                t--;
+    public static void solve(char[] P , char[] T, int[] fail){
+        int pl = P.length;
+        int tl = T.length;
+        int answer = 0 ;
+        StringBuilder sb = new StringBuilder();
+        int pIdx = 0 ;
+        for(int i = 0 ; i < tl ; i ++){
+            if(P[pIdx] == T[i]){
+                if(pIdx == pl-1){
+                    answer++;
+                    sb.append((i - pIdx + 1)).append(" ");
+                    pIdx = fail[pIdx];
+                }
+                else{
+                    pIdx ++;
+                }
             }
-            if(P[t] == P[i]) {
-                fail[i] = t+1;
+            else{
+                while(pIdx > 0 && T[i] != P[pIdx]){
+                    pIdx = fail[pIdx-1];
+                }
+                if(T[i] == P[pIdx]){
+                    pIdx ++;
+                }
+            }
+        }
+        System.out.println(answer);
+        System.out.println(sb.toString());
+    }
+    public static void failFunction(char[] P,int[] fail){
+        int j = 0 ;
+        int pl = P.length;
+        for(int i = 1 ; i < pl ; i ++){
+            while(j > 0 && P[i] != P[j]){
+                j = fail[j-1];
+            }
+            if(P[j] == P[i]){
+                j ++;
+                fail[i] = j;
+            }
+            else{
+                fail[i] = 0;
             }
         }
     }
+
 }
