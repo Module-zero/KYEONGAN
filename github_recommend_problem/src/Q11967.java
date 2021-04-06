6ac1e942e709dd7b01539026b6695942b0e9fb62
jeonka1001 <ruddks2009@naver.com>
2021-04-06 20:02:41 +0900

    2021-04-06 -1

---
+++ github_recommend_problem/src/Q11967.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q11967 {
+    public static void main(String[]args)throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] init = bf.readLine().split(" ");
+        int N = Integer.parseInt(init[0]);
+        int M = Integer.parseInt(init[1]);
+
+
+    }
+}
