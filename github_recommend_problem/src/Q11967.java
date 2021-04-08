fd84c4a532b1ed8f94d05eba229c8e3b3d82053a
jeonka1001 <ruddks2009@naver.com>
2021-04-08 10:51:55 +0900

    2021-04-08 -1

--- github_recommend_problem/src/Q11967.java
+++ github_recommend_problem/src/Q11967.java
@@ -8,7 +8,13 @@ public class Q11967 {
         String[] init = bf.readLine().split(" ");
         int N = Integer.parseInt(init[0]);
         int M = Integer.parseInt(init[1]);
-
+        for(int i = 0 ; i < M ; i ++){
+            String[] tmp = bf.readLine().split(" ");
+            int x = Integer.parseInt(tmp[0]);
+            int y = Integer.parseInt(tmp[1]);
+            int a = Integer.parseInt(tmp[2]);
+            int b = Integer.parseInt(tmp[3]);
+        }
 
     }
 }
