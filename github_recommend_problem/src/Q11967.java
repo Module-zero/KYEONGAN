b2ea0339ed210a19f8820fc830a2d6595e2904f1
jeonka1001 <ruddks2009@naver.com>
2021-04-11 23:16:42 +0900

    2021-04-11 -1 : 메모리 초과

--- github_recommend_problem/src/Q11967.java
+++ github_recommend_problem/src/Q11967.java
@@ -8,11 +8,14 @@ import java.util.LinkedList;
 
 public class Q11967 {
     static LinkedList<int[]>[][] map ;
+    static boolean[][] turn ;
+    static int N;
     public static void main(String[]args)throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] init = bf.readLine().split(" ");
-        int N = Integer.parseInt(init[0]);
+        N = Integer.parseInt(init[0]);
         map = new LinkedList[N+1][N+1];
+        turn = new boolean[N+1][N+1];
         for(int i = 1 ; i <= N ; i ++){
             for(int j = 1 ; j <= N ; j ++){
                 map[i][j] = new LinkedList<>();
@@ -27,19 +30,37 @@ public class Q11967 {
             int b = Integer.parseInt(tmp[3]) ;
             map[x][y].add(new int[]{a,b});
         }
-        LinkedList<int[]> queue = new LinkedList<>();
-        boolean[][] light = new boolean[N+1][N+1];
-        int curX = 1;
-        int curY = 1;
-        light[curX][curY] = true;
         int ans = 1;
-        while(!map[curX][curY].isEmpty()){
-            int[] idx = map[curX][curY].remove();
-
-
+        int res ;
+        do{
+            res = bfs();
+            ans += res;
+        }while(res != 0);
+        System.out.println(ans);
+    }
+    public static int bfs(){
+        int[][] move = {{1,0},{0,1}};
+        int res = 0 ;
+        LinkedList<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{1,1});
+        while(!queue.isEmpty()){
+            int[] idx = queue.remove();
+            while(!map[idx[0]][idx[1]].isEmpty()){
+                int[] light = map[idx[0]][idx[1]].remove();
+                if(!turn[light[0]][light[1]]) {
+                    turn[light[0]][light[1]] = true;
+                    res += 1;
+                }
+            }
+            for(int i = 0 ; i < 2 ; i ++){
+                int curX = idx[0]+move[i][0];
+                int curY = idx[1]+move[i][1];
+                if(curX > N|| curY > N)continue;
+                if(!turn[curX][curY])continue;
+                queue.add(new int[]{curX,curY});
+            }
         }
-        StringBuilder sb = new StringBuilder();
-
+        return res;
     }
 
 
