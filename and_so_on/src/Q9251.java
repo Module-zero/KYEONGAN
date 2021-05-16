100ef4f2e2253f5164e41a31d1c5597f7dda662d
jeonka1001 <ruddks2009@naver.com>
2021-05-16 20:35:16 +0900

    최장 공통 부분 수열

--- and_so_on/src/Q9251.java
+++ and_so_on/src/Q9251.java
@@ -8,16 +8,21 @@ public class Q9251 {
         String str2 = bf.readLine();
         int len1 = str1.length();
         int len2 = str2.length();
-        int[][] arr = new int[len1][len2];
-        for(int i = 0 ; i < len1 ; i ++){
-            char target = str1.charAt(i);
-            int num = 0;
-            for(int j = 0 ; j < len2 ; j ++){
-                char comp = str2.charAt(j);
-                if(comp == target){
+        int[][] LCS = new int[len1+1][len2+1];
+        int max = 0;
+        for( int i = 1 ; i <= len1 ; i ++){
+            char target = str1.charAt(i-1);
 
+            for(int j = 1 ; j <= len2 ; j++){
+                if(target == str2.charAt(j-1)){
+                    LCS[i][j] = LCS[i-1][j-1] + 1;
                 }
+                else{
+                    LCS[i][j] = Math.max(LCS[i-1][j],LCS[i][j-1]);
+                }
+                max = Math.max(LCS[i][j],max);
             }
         }
+        System.out.println(max);
     }
 }
